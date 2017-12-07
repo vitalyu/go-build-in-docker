b957e5cb769af9accf69285d05055bfce44687b9
Vitaly Uvarov <vitalyu@gmail.com>
2017-12-07 15:56:53 +0300

    color variables

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ echo -e "${COLOR_PURPLE}--- Building project from${COLOR_N} ${SRC_DIR}"
 docker run --rm -i \
   -v "${SRC_DIR}":"${SRC_DIR}" \
   -w "${SRC_DIR}" \
-  -e COLOR_PURPLE='\033[0;35m' \
-  -e COLOR_CYAN='\033[0;36m' \
-  -e COLOR_N='\033[0m' \
+  -e COLOR_PURPLE="${COLOR_PURPLE}" \
+  -e COLOR_CYAN="${COLOR_CYAN}" \
+  -e COLOR_N="${COLOR_N}" \
 golang:1.8 bash <<'EOF'
 
 ###---------------------------
@@ -24,7 +24,7 @@ golang:1.8 bash <<'EOF'
 echo -e "${COLOR_PURPLE}--- Downloading project dependencies ${COLOR_N}"
 go get -d
 
-for GOOS in darwin linux; do 	# darwin linux win
+for GOOS in darwin; do 	# darwin linux win
   for GOARCH in amd64; do 	# 386 amd64
     echo -e "${COLOR_PURPLE}--- Building for${COLOR_CYAN} ${GOOS} ${GOARCH} ${COLOR_N} "
     export GOOS=$GOOS
